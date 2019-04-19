df850f6ecff1d25d9715e95d523e11260aa1b855
DPANET <45304524+DPANET@users.noreply.github.com>
2019-04-19 23:28:37 +0400

    update

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,5 +1,39 @@
 
+import $ from "jquery";
+import * as prayerlib from '@dpanet/prayers-lib';
+import { isNullOrUndefined } from 'util';
+import cg = require("./configurators/configuration");
+
 $("#submit-button").click(()=> { 
     alert('hi');
 
 });
+
+async function buildObject()
+{
+    try{
+
+
+        let prayerConfig: prayerlib.IPrayersConfig;
+        let locationConfig: prayerlib.ILocationConfig;
+        
+
+        prayerConfig= await new prayerlib.Configurator().getPrayerConfig();
+        locationConfig = await new prayerlib.Configurator().getLocationConfig();
+        appmanager._prayerManager = await prayerlib.PrayerTimeBuilder
+            .createPrayerTimeBuilder(null, appmanager._prayerConfig)
+            .setPrayerMethod(prayerlib.Methods.Mecca)
+          //  .setPrayerPeriod(prayerlib.DateUtil.getNowDate(), prayerlib.DateUtil.addDay(1, prayerlib.DateUtil.getNowDate()))
+            .setLocationByCoordinates(Homey.ManagerGeolocation.getLatitude(), Homey.ManagerGeolocation.getLongitude())
+            .createPrayerTimeManager();
+        appmanager.initPrayersSchedules();
+        appmanager.initEvents();
+        console.log(appmanager._prayerManager.getUpcomingPrayer());
+
+    }
+    catch(err)
+    {
+
+    }
+
+}
