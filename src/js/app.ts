eef86ef0172b87423938701ff01326bcf5c6508c
dpanet <dpanet@hotmail.com>
2019-04-22 07:22:56 +0400

    updated prayerlib

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,6 +1,7 @@
 
 import prayerlib = require("@dpanet/prayers-lib");
 import * as joi from "joi";
+import { PrayersName } from "@dpanet/prayers-lib";
 
 buildObject();
 export async function buildObject()
@@ -8,14 +9,15 @@ export async function buildObject()
     try{
 
 
-        // let prayerConfig: prayerlib.IPrayersConfig = await new prayerlib.Configurator().getPrayerConfig();
-        // let locationConfig: prayerlib.ILocationConfig = await new prayerlib.Configurator().getLocationConfig();
-        // console.log(locationConfig);
-        // let prayerManager: prayerlib.IPrayerManager = await prayerlib.PrayerTimeBuilder
-        //     .createPrayerTimeBuilder(locationConfig, prayerConfig)
-        //     .createPrayerTimeManager();
+        let prayerConfig: prayerlib.IPrayersConfig = await new prayerlib.Configurator().getPrayerConfig();
+        let locationConfig: prayerlib.ILocationConfig = await new prayerlib.Configurator().getLocationConfig();
+        console.log(locationConfig);
+        let prayerManager: prayerlib.IPrayerManager = await prayerlib.PrayerTimeBuilder
+            .createPrayerTimeBuilder(locationConfig, prayerConfig)
+            .createPrayerTimeManager();
         
-        // let config:prayerlib.IConfig   = new prayerlib.Configurator();
+        let config:prayerlib.IConfig   = new prayerlib.Configurator();
+        
         $("#fajr-time").val(3);
         $("#submit-button").on("click",()=>{alert('Hi');});
     }
