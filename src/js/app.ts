9eb4b4e7ed83c9b7aa6b3987b50e7df1c89a5532
dpanet <dpanet@hotmail.com>
2019-04-23 06:17:54 +0400

    grace-fs

--- src/js/app.ts
+++ src/js/app.ts
@@ -9,11 +9,12 @@ export async function buildObject()
 
         let prayerConfig: prayerlib.IPrayersConfig = await new prayerlib.Configurator().getPrayerConfig();
         let locationConfig: prayerlib.ILocationConfig = await new prayerlib.Configurator().getLocationConfig();
+        console.log(locationConfig);
         let prayerManager: prayerlib.IPrayerManager = await prayerlib.PrayerTimeBuilder
             .createPrayerTimeBuilder(locationConfig, prayerConfig)
             .createPrayerTimeManager();
-        
-        let config:prayerlib.IConfig   = new prayerlib.Configurator();
+        console.log(prayerManager.getPrayerAdjustmentsByPrayer(prayerlib.PrayersName.FAJR));
+        //let config:prayerlib.IConfig   = new prayerlib.Configurator();
         let fajrAdjustment:number  = prayerManager.getPrayerAdjustmentsByPrayer(prayerlib.PrayersName.FAJR).adjustments;
         
         console.log(fajrAdjustment);
