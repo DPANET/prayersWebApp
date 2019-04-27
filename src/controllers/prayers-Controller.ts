1f7af554ea17056abf530d3bdd99fd50927e519c
dpanet <dpanet@hotmail.com>
2019-04-27 09:18:39 +0400

    express

---
+++ src/controllers/prayers-Controller.ts
@@ -0,0 +1,22 @@
+import * as prayerlib from "@dpanet/prayers-lib";
+
+export class PrayersController
+{
+    private _prayersController:PrayersController;
+
+    constructor()
+    {// {
+    //             let prayerConfig: prayerlib.IPrayersConfig = await new prayerlib.Configurator().getPrayerConfig();
+    //     let locationConfig: prayerlib.ILocationConfig = await new prayerlib.Configurator().getLocationConfig();
+    //     console.log(locationConfig);
+    //     let prayerManager: prayerlib.IPrayerManager = await prayerlib.PrayerTimeBuilder
+    //         .createPrayerTimeBuilder(locationConfig, prayerConfig)
+    //         .createPrayerTimeManager();
+    }
+    static getPrayerController(): PrayersController
+    {
+
+        return ;
+
+    }
+}
