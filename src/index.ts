9dbe7d4720925bc77dba6b7192a8f9f4b036aaa8
dpanet <dpanet@hotmail.com>
2019-04-29 07:29:07 +0400

    frontend compilation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import mainRouter  from "./routes/main.router";
 import prayersController from "./controllers/prayers.controller";
 import mainController from "./controllers/main.controller";
-
+import * as request from 'request-promise-native';
+import { domainMatch } from "tough-cookie";
+const to = require('await-to-js').default;
 const app:mainRouter = new mainRouter([new prayersController(),new mainController()]);
-app.listen();
+ app.listen();
+
+// setTimeout(()=>{doSomething()}, 5000);
+// async function  doSomething()
+// {        let err:Error, result: any, url: any;
+
+// let queryString: any =
+// {
+//     uri: 'http://localhost:3005/PrayerManager/PrayersAdjustments/',
+//     method: 'GET',
+//     json: true,
+//     resolveWithFullResponse: false
+
+// };
+
+// [err, result] = await to(request.get(queryString));
+// console.log(result);
+// console.log("Error: "+err);
+// }
--- src/public/js/settings.ts
+++ src/public/js/settings.ts
@@ -8,35 +8,36 @@ import * as prayerlib from "../../models/prayers.model";
 export async function buildObject()
 {
     try{
-        // let prayerConfig: prayerlib.IPrayersConfig = await new prayerlib.Configurator().getPrayerConfig();
-        // let locationConfig: prayerlib.ILocationConfig = await new prayerlib.Configurator().getLocationConfig();
-        // console.log(locationConfig);
-        // let prayerManager: prayerlib.IPrayerManager = await prayerlib.PrayerTimeBuilder
-        //     .createPrayerTimeBuilder(locationConfig, prayerConfig)
-        //     .createPrayerTimeManager();
-        // console.log(prayerManager.getPrayerAdjustmentsByPrayer(prayerlib.PrayersName.FAJR));
-        // //let config:prayerlib.IConfig   = new prayerlib.Configurator();
-        // let fajrAdjustment:number  = prayerManager.getPrayerAdjustmentsByPrayer(prayerlib.PrayersName.FAJR).adjustments;
-        
-        // // console.log(fajrAdjustment);
-     
-        //  let err: Error, result: any;
-        //  await getDB().then(result => result.get('config.prayerConfig.calculations').value());
    
         $("#submit-button").on("click", () => { 
-            $.ajax({ url: "PrayerManager/PrayersAdjustments/", success: function (result: any)
+            $.ajax({ url: "PrayerManager/PrayersAdjustments/", success: (result)=>
             {
-                let prayerAdjustment:prayerlib.PrayerAdjustment[] = result;
-                $('fajr-time').val(prayerAdjustment[1].adjustments);
-                $('dhur-time').val(prayerAdjustment[2].adjustments);
-                alert($('fajr-time').val(prayerAdjustment[1].adjustments));
-                
-            } })}); 
+                loadPrayerAdjustments(result);
+            }})}); 
     }
     catch(err)
     {
        alert(err);
     }
+}
+function loadPrayerAdjustments(prayerAdjustment:prayerlib.PrayerAdjustment[])
+{
+
+    prayerAdjustment.forEach(element => {
+        switch(element.prayerName)
+        {
+            case "Fajr": $("#fajr-time").val(element.adjustments);
+            break;
+            case "Dhuhr":  $("#dhur-time").val(element.adjustments);
+            break;
+            case "Asr": $("#asr-time").val(element.adjustments);
+            break;
+            case "Maghrib": $("#maghrib-time").val(element.adjustments);
+            break;
+            case "Isha": $("#isha-time").val(element.adjustments);
+            break;
+        }                    
+    });
 }
 buildObject();
 //  async function  getDB(): Promise<lowdb.LowdbAsync<any>> {
