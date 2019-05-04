d3173f4e0733154d4e21fe043342be938d7287e1
dpanet <dpanet@hotmail.com>
2019-05-04 23:24:11 +0400

    mobile table

--- src/public/js/settings.ts
+++ src/public/js/settings.ts
@@ -11,7 +11,7 @@ const  $  =require( 'jquery');
 const DataTable = require("datatables.net")(window, $);
 const daterangepicker = require("daterangepicker");
 const DataTableResp = require("datatables.net-responsive")(window, $);
-
+const DataTableRowGroup = require("datatables.net-rowgroup")(window, $);
 
 interface IPrayersView {
     prayerDate: string,
@@ -45,47 +45,50 @@ export async function buildObject() {
     }
 }
 function loadDataTable() {
-    $('#prayers-table').show(300);
+    $('#prayers-table-mobile').show(300);
 
     $.ajax({
         url: "PrayerManager/Prayers", success: (prayers: prayerlib.IPrayers[]) => {
             let prayersDataTable: IPrayersView[] = getPrayerView(prayers);
-        //   let prayerDataTableMobile: IPrayersViewRow[] = getPrayerViewRow(prayersDataTable);
-          // console.log(prayerDataTableMobile);
-            $('#prayers-table').DataTable(
-                {
-                    data: prayersDataTable,
-                    searching: false,
-                    paging: false,
-                    ordering: false,
-                    responsive: true,
-                    columns: [
-                        { data: 'prayerDate', responsivePriority: 1 },
-                        { data: 'fajr', responsivePriority: 2 },
-                        { data: 'sunrise', responsivePriority: 9 },
-                        { data: 'dhuhr', responsivePriority: 3 },
-                        { data: 'asr', responsivePriority: 4 },
-                        { data: 'sunrise', responsivePriority: 8 },
-                        { data: 'maghrib', responsivePriority: 5 },
-                        { data: 'isha', responsivePriority: 6 },
-                        { data: 'midnight', responsivePriority: 7 }
-                    ]
-                }
-            )
-            // $('#prayers-table-mobile').DataTable(
+            let prayerDataTableMobile: IPrayersViewRow[] = getPrayerViewRow(prayersDataTable);
+            // $('#prayers-table').DataTable(
             //     {
-            //         data: prayerDataTableMobile,
+            //         data: prayersDataTable,
             //         searching: false,
             //         paging: false,
             //         ordering: false,
             //         responsive: true,
             //         columns: [
-            //             { data: 'Date' },
-            //             { data: 'Prayer' },
-            //             { data: 'Time' }
+            //             { data: 'prayerDate', responsivePriority: 1 },
+            //             { data: 'fajr', responsivePriority: 2 },
+            //             { data: 'sunrise', responsivePriority: 9 },
+            //             { data: 'dhuhr', responsivePriority: 3 },
+            //             { data: 'asr', responsivePriority: 4 },
+            //             { data: 'sunrise', responsivePriority: 8 },
+            //             { data: 'maghrib', responsivePriority: 5 },
+            //             { data: 'isha', responsivePriority: 6 },
+            //             { data: 'midnight', responsivePriority: 7 }
             //         ]
             //     }
             // )
+            $('#prayers-table-mobile').DataTable(
+                {
+                    data: prayerDataTableMobile,
+                    searching: false,
+                    paging: false,
+                    ordering: false,
+                    responsive: true,
+                    rowGroup:
+                    {
+                        dataSrc:'prayerDate'
+                    },
+                    
+                    columns: [
+                        { data: 'prayerName',responsivePriority:2, className:"th"},
+                        { data: 'prayerTime' ,responsivePriority:3, className:"th"}
+                    ]
+                }
+            )
 }
     });
 }
@@ -99,7 +102,7 @@ function getPrayerView(prayers: prayerlib.IPrayers[]): IPrayersView[] {
         });
         prayerViewObject =
             {
-                prayerDate: moment(curr.prayersDate).format('L'),
+                prayerDate: moment(curr.prayersDate).format('YYYY-MM-DD'),
                 fajr: moment(prayerTimings[0]).format('LT'),
                 sunrise: moment(prayerTimings[1]).format('LT'),
                 dhuhr: moment(prayerTimings[2]).format('LT'),
@@ -117,7 +120,7 @@ function getPrayerView(prayers: prayerlib.IPrayers[]): IPrayersView[] {
 function getPrayerViewRow(prayersView: IPrayersView[]): IPrayersViewRow[] {
     let prayerViewRow: Array<IPrayersViewRow> = new Array<IPrayersViewRow>();
     prayersView.forEach((prayerViewObject, index, arr) => {
-        prayerViewRow.concat([
+        prayerViewRow.push(
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.FAJR, prayerTime: prayerViewObject.fajr },
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.SUNRISE, prayerTime: prayerViewObject.sunrise },
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.DHUHR, prayerTime: prayerViewObject.dhuhr },
@@ -126,7 +129,7 @@ function getPrayerViewRow(prayersView: IPrayersView[]): IPrayersViewRow[] {
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.MAGHRIB, prayerTime: prayerViewObject.maghrib },
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.ISHA, prayerTime: prayerViewObject.isha },
             { prayerDate: prayerViewObject.prayerDate, prayerName: prayerlib.PrayersName.MIDNIGHT, prayerTime: prayerViewObject.midnight },
-        ]);
+        );
     });
 
     return prayerViewRow;
