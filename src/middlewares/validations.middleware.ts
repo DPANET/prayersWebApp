277598b824d503b1edb2dc8a18bdcefb393973a2
omar <dpanet@hotmail.com>
2019-05-24 09:34:55 +0400

    exception

--- src/middlewares/validations.middleware.ts
+++ src/middlewares/validations.middleware.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as exceptionHandler from '../exceptions/exception.handler';
-
+import * as val from '@dpanet/prayers-lib/lib/validators/validator'
+import valiadtor = val.validators;
 export class ValidationMiddleware
 {
     constructor()
