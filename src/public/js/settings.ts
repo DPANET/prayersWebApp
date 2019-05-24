e46a3f662569d72666e4fceca790882104e99487
dpanet <dpanet@hotmail.com>
2019-05-24 18:11:12 +0400

    noty faulty

--- src/public/js/settings.ts
+++ src/public/js/settings.ts
@@ -3,7 +3,7 @@
 // import lowdbfile from "lowdb/adapters/FileAsync";
 // const to = require('await-to-js').default;
 // import _ = require('lodash');
-import $ from 'jquery';
+import $ = require('jquery');
 import * as prayerlib from "../../models/prayers.model";
 //import daterangepicker from "daterangepicker";
 import moment from "moment";
@@ -14,7 +14,7 @@ const DataTable = require("datatables.net")(window, $);
 const daterangepicker =require( "daterangepicker");
 const DataTableResp = require("datatables.net-responsive")(window, $);
 const DataTableRowGroup = require("datatables.net-rowgroup")(window, $);
-
+import noty = require("noty");
 export async function buildObject() {
     try {
 
@@ -22,19 +22,57 @@ export async function buildObject() {
        await $('document').ready(async () => {
             await loadPrayerAdjustments();
             await loadPrayerPrayerSettings();
-            //await loadDataTable();
+            loadNotification();
             $("#view-button").on("click", refreshDataTable);
-    //        $("#view-button").on("click", refreshParams);
-//             refreshParams();
-            //$("#submit-button").on("click",saveDataTable);
+            
         }
         );
 
     }
     catch (err) {
-        alert(err);
+    //    alert(err);
+        $.noty.setText("Error",err.message);
     }
 }
+function loadNotification():any {
+    $.noty.defaults = {
+        layout: 'top',
+        theme: 'bootstrapTheme', // or relax
+        type: 'error', // success, error, warning, information, notification
+        text: '', // [string|html] can be HTML or STRING
+      
+        dismissQueue: true, // [boolean] If you want to use queue feature set this true
+        force: false, // [boolean] adds notification to the beginning of queue when set to true
+        maxVisible: 5, // [integer] you can set max visible notification count for dismissQueue true option,
+      
+        template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+      
+        timeout: false, // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
+        progressBar: false, // [boolean] - displays a progress bar
+      
+        animation: {
+          open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+          close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+          easing: 'swing',
+          speed: 500 // opening & closing animation speed
+        },
+        closeWith: ['click'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+      
+        modal: false, // [boolean] if true adds an overlay
+        killer: false, // [boolean] if true closes all notifications and shows itself
+      
+        callback: {
+          onShow: function() {},
+          afterShow: function() {},
+          onClose: function() {},
+          afterClose: function() {},
+          onCloseClick: function() {},
+        },
+      
+        buttons: false // [boolean|array] an array of buttons, for creating confirmation dialogs.
+      };
+}
+
 function refreshParams():any
 {
     let prayerAdjustment:prayerlib.IPrayerAdjustments[]= new Array<prayerlib.IPrayerAdjustments>();
