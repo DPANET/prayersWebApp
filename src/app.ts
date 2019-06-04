a9a80ae95b1625452379929baaf0869be0a14820
dpanet <dpanet@hotmail.com>
2019-06-04 08:12:51 +0400

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import  {App} from "./routes/main.router";
 import prayersController from "./controllers/prayers.controller";
 import mainController from "./controllers/main.controller";
+
 import * as  events from "./events/events";
 const to = require('await-to-js').default;
 let app:App = new App([new prayersController(),new mainController()]);
